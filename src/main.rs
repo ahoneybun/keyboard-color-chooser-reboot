22311815fe2a774b973fcad647386bf95a57333d
Aaron Honeycutt <honeycuttaaron3@gmail.com>
2019-06-25 10:20:09 -0600

    fix some containers

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate gtk;
 use gio::prelude::*;
 use gtk::prelude::*;
 
-use gtk::{Box, ContainerExt, WidgetExt};
+//use gtk::{Box, ContainerExt, WidgetExt};
 
 use std::env::args;
 
@@ -16,12 +16,26 @@ fn build_ui(application: &gtk::Application) {
     window.set_position(gtk::WindowPosition::Center);
     window.set_default_size(500, 150);
 
+    let box = Box::new(gtk::Orientation::Vertical, 12);
+    window.add(&box);
+
+    // Buttons
     let left = gtk::Button::new_with_label("Left");
     let center = gtk::Button::new_with_label("Center");
     let right = gtk::Button::new_with_label("Right");
 
     let container = Box::new(gtk::Orientation::Horizontal, 50);
-    window.add(&container);
+    box.add(&container);
+    
+
+    // Labels
+    let label = gtk::Label::new(None);
+    label.set_text("Left");
+    
+    let container2 = Box::new(gtk::Orientation::Horizontal, 100);
+    box.add(&container2);
+    
+    container2.add(&label);
 
     container.add(&left);
     container.add(&center);
@@ -32,7 +46,7 @@ fn build_ui(application: &gtk::Application) {
 
 fn main() {
     let application =
-        gtk::Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
+        gtk::Application::new(Some("com.github.ahoneybun.keyboard-color-chooser"), Default::default())
             .expect("Initialization failed...");
 
     application.connect_activate(|app| {
