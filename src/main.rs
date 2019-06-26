5da38ec832ce273995c67f380d6fd4dd7d578f42
Aaron Honeycutt <honeycuttaaron3@gmail.com>
2019-06-25 18:24:09 -0600

    some work on using GridExt

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use gio::prelude::*;
 use gtk::prelude::*;
 
 //use gtk::{Box, ContainerExt, WidgetExt};
+use gtk::{GridExt};
 
 use std::env::args;
 
@@ -16,30 +17,21 @@ fn build_ui(application: &gtk::Application) {
     window.set_position(gtk::WindowPosition::Center);
     window.set_default_size(500, 150);
 
-    let box = Box::new(gtk::Orientation::Vertical, 12);
-    window.add(&box);
-
     // Buttons
-    let left = gtk::Button::new_with_label("Left");
-    let center = gtk::Button::new_with_label("Center");
-    let right = gtk::Button::new_with_label("Right");
+    let leftButton = gtk::Button::new_with_label("Left");
+    let centerButton = gtk::Button::new_with_label("Center");
+    let rightButton = gtk::Button::new_with_label("Right");
 
-    let container = Box::new(gtk::Orientation::Horizontal, 50);
-    box.add(&container);
-    
+    let grid = GridExt:;
+    window.add(&grid);
 
     // Labels
-    let label = gtk::Label::new(None);
-    label.set_text("Left");
+    //let label = gtk::Label::new(None);
+    //label.set_text("Left");
     
-    let container2 = Box::new(gtk::Orientation::Horizontal, 100);
-    box.add(&container2);
-    
-    container2.add(&label);
-
-    container.add(&left);
-    container.add(&center);
-    container.add(&right);
+    grid.add(&leftButton);
+    grid.add(&centerButton);
+    grid.add(&rightButton);
 
     window.show_all();
 }
